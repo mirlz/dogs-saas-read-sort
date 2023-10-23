05f394cb3d39a350398e98d046bafd947a8131a8
Eugenia <mirlzy@gmail.com>
2023-10-23 07:27:04 +0000

    Update 3 files

--- src/components/DoggoSearch.jsx
+++ src/components/DoggoSearch.jsx
@@ -1,48 +1,45 @@
-import { useMemo } from 'react';
-import { Input, Form } from 'antd';
+import { useMemo } from "react";
+import { Input, Form } from "antd";
 import { debounce } from "lodash";
-import axios from 'axios';
-import apis from '../store/api.json';
+import axios from "axios";
+import apis from "../store/api.json";
 
-const DoggoSearch = ((props) => {
+const DoggoSearch = (props) => {
+  const fetchDogs = (searchTerm) => {
+    console.log(import.meta.env.VITE_DOGAPI_KEY);
+    props.setLoading(true);
+    axios({
+      method: "GET",
+      url: `${apis.getBreeds}q=${searchTerm}`,
+      headers: {
+        "x-api-key": import.meta.env.VITE_DOGAPI_KEY,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        props.setDogs(response.data);
+        props.setLoading(false);
+      })
+      .catch((err) => {
+        console.log("fetch doggos err: ", JSON.stringify(err));
+        props.setError(true);
+        props.setLoading(false);
+      });
+  };
 
-    const fetchDogs = ((searchTerm) => {
-        console.log(searchTerm)
-        props.setLoading(true);
-        axios({
-            method: "GET",
-            url: `${apis.getBreeds}q=${searchTerm}`,
-            headers: {
-                'x-api-key': import.meta.env.VITE_DOGAPI_KEY,
-                'Content-Type': 'application/json'
-            }
-        }).then((response) => {
-            props.setDogs(response.data);
-            props.setLoading(false);
-        }).catch((err) => {
-            console.log('fetch doggos err: ', JSON.stringify(err));
-            props.setError(true);
-            props.setLoading(false);
-        });
-    });
+  const debounced = useMemo(() => debounce(fetchDogs, 1000), []);
 
-    const debounced = useMemo(() => debounce(fetchDogs, 1000), []);
-
-    return (
-        <Form.Item
-            name="breed-search"
-            label="Breed Name"
-            field="breed-search"
-        >
-            <Input
-                allowClear={true}
-                placeholder="input search text"
-                onChange={(e) => {
-                    debounced(e.target.value);
-                }}
-            />
-        </Form.Item>
-    );
-});
+  return (
+    <Form.Item name="breed-search" label="Breed Name" field="breed-search">
+      <Input
+        allowClear={true}
+        placeholder="input search text"
+        onChange={(e) => {
+          debounced(e.target.value);
+        }}
+      />
+    </Form.Item>
+  );
+};
 
 export default DoggoSearch;
